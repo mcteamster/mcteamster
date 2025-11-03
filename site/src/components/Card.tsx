4f88911d6679e3566b5e7aed49ca5dac11bfb68f
mcteamster <mcteamster@outlook.com>
2025-11-03 12:40:08 +1100

    feat: round favicon

--- site/src/components/Card.tsx
+++ site/src/components/Card.tsx
@@ -17,7 +17,7 @@ interface CardProps {
 function Card(props: CardProps) {
   return (
     <>
-      <div className={`${props.data.colour || 'bg-slate-200'} w-full rounded-lg border shadow-sm shadow-stone-950/5 max-w-[20rem] overflow-hidden m-5`}>
+      <div className={`${props.data.colour || 'bg-slate-100'} w-full rounded-lg border shadow-sm shadow-stone-950/5 max-w-[20rem] overflow-hidden m-5`}>
         <img src={props.data.image} alt="ui/ux review check" className="w-[calc(100%-16px)] h-max rounded m-2" />
         <div className="w-full h-max rounded px-3.5 py-2.5 text-center">
           <h6 className="font-sans antialiased font-bold text-base md:text-lg lg:text-xl text-current">
--- site/src/components/Navbar.tsx
+++ site/src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 function Navbar() {
   return <>
-    <nav className="fixed top-0 border shadow-lg overflow-hidden p-1 bg-slate-200 border-stone-200 shadow-stone-950/5 w-full max-w-screen-xl">
+    <nav className="fixed top-0 z-10 border shadow-lg overflow-hidden p-1 bg-slate-100 border-stone-200 shadow-stone-950/5 w-full max-w-screen-xl">
       <div className="flex items-center">
         <a href="#" className="font-sans antialiased text-sm text-current mx-2 block py-1 font-semibold">
-          Games by Mcteamster
+          games by mcteamster
         </a>
         <div className='ml-auto mr-2 block'>
           <ul className="m-2 flex flex-row gap-x-3 gap-y-1 lg:m-0 lg:flex-row lg:items-center underline">
