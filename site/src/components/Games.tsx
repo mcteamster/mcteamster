b76552dbada229035335d8edbd1845865a69e401
mcteamster <28884260+mcteamster@users.noreply.github.com>
2025-11-06 23:23:09 +1100

    feat: adjust ratios

--- site/src/components/Games.tsx
+++ site/src/components/Games.tsx
@@ -4,7 +4,7 @@ import { gameData } from '../data/games'
 function Games() {
   return (
     <>
-      <div className='overflow-scroll flex flex-wrap justify-center items-center pt-15 pb-10 h-[100vh]'>
+      <div className='no-scrollbar overflow-y-scroll flex flex-wrap justify-center items-center pt-15 h-[100vh]'>
         {gameData.games.map((game, index) => (
           <Card key={index} data={game} />
         ))}
