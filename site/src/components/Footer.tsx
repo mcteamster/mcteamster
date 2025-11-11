4cf1f2fbe04014d94290c332d4934334abfd8bac
mcteamster <28884260+mcteamster@users.noreply.github.com>
2025-11-11 22:29:41 +1100

    feat: black header

--- site/src/components/Footer.tsx
+++ site/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return <>
-    <div className="fixed bottom-0 z-10 h-10 bg-linear-[0deg,rgba(0,0,0,1),rgba(0,0,0,0)] w-full"></div>
+    <div className="fixed bottom-0 z-10 h-10 w-full bg-linear-[0deg,black,rgba(0,0,0,0)]"></div>
   </>
 }
 
--- site/src/components/Navbar.tsx
+++ site/src/components/Navbar.tsx
@@ -1,23 +1,23 @@
 function Navbar() {
   return <>
-    <nav className="fixed top-0 z-10 shadow-lg overflow-hidden p-1 bg-slate-100 shadow-stone-950/5 w-full">
+    <nav className="fixed z-10 p-1 w-full bg-linear-[180deg,black,black,85%,rgba(0,0,0,0)] text-white ">
       <div className="flex items-center">
-        <a href="#" className="font-sans antialiased text-sm text-current mx-2 block py-1 font-semibold">
-          games by mcteamster
+        <a href="#" className="font-sans antialiased text-md text-current mx-2 block py-1 font-semibold">
+            games by mcteamster
         </a>
-        <div className='ml-auto mr-2 block'>
-          <ul className="m-2 flex flex-row gap-x-3 gap-y-1 lg:m-0 lg:flex-row lg:items-center underline">
+        <div className='ml-auto mr-1 block'>  
+          <ul className="m-1 flex flex-row gap-x-3 gap-y-1 lg:m-0 lg:flex-row lg:items-center font-semibold">
             <li>
-              <a href="https://github.com/mcteamster" className="font-sans antialiased text-sm text-current p-1 hover:text-primary">GitHub</a>
+              <a href="https://github.com/mcteamster" className="font-sans antialiased text-sm text-current p-1 hover:text-primary underline">GitHub</a>
             </li>
             <li>
-              <a href="https://mcteamster.itch.io" className="font-sans antialiased text-sm text-current p-1 hover:text-primary">Itch.io</a>
+              <a href="https://mcteamster.itch.io" className="font-sans antialiased text-sm text-current p-1 hover:text-primary underline">Itch.io</a>
             </li>
             <li>
-              <a href="https://bsky.app/profile/mcteamster.com" className="font-sans antialiased text-sm text-current p-1 hover:text-primary">Bsky</a>
+              <a href="https://www.buymeacoffee.com/mcteamster" className="font-sans antialiased text-sm text-current p-1 hover:text-primary underline">Support</a>
             </li>
             <li>
-              <a href="https://www.buymeacoffee.com/mcteamster" className="font-sans antialiased text-sm text-current p-1 hover:text-primary">Support</a>
+              <a href="https://bsky.app/profile/mcteamster.com" className="font-sans antialiased text-md text-current p-1 hover:text-primary">&#x1F98B;</a>
             </li>
           </ul>
         </div>
