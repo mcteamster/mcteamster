f9ae878c136266625642fcee112f76ba610e5497
mcteamster <28884260+mcteamster@users.noreply.github.com>
2025-11-16 12:28:35 +1100

    feat: adjust card size

--- site/src/components/Card.tsx
+++ site/src/components/Card.tsx
@@ -17,39 +17,37 @@ interface CardProps {
 function Card(props: CardProps) {
   return (
     <>
-      <div className={`${props.data.colour || 'bg-slate-100'} w-full rounded-lg shadow-xl shadow-black max-w-[21rem] overflow-hidden m-5 z-5`}>
-        <a href={props.data.link} target="_blank" rel="noopener noreferrer">
-          <img src={props.data.image} loading="lazy" className="w-[calc(100%-16px)] aspect-16/9 rounded m-2" />
-        </a>
-        <div className="w-full h-max rounded px-3.5 py-2.5 text-center">
-          <h6 className="font-sans antialiased font-bold text-base md:text-lg lg:text-xl text-current">
-            {props.data.title}
-          </h6>
-          <small className="font-sans antialiased text-sm text-current">
-            {props.data.date}
-          </small>
-          <p className="font-sans antialiased text-base mt-1 text-stone-600 min-h-[3rem]">
-            {props.data.description}
-          </p>
-        </div>
-        <div className="w-full px-3.5 pt-2 pb-3.5 rounded mt-4 flex items-center justify-center">
-          <div className="flex items-center">
-            <a href={props.data.link} target="_blank" rel="noopener noreferrer" className="m-1 cursor-pointer inline-flex items-center justify-center border align-middle select-none font-sans font-medium text-center duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:cursor-not-allowed focus:shadow-none text-sm py-2 px-4 shadow-sm hover:shadow-md bg-stone-800 hover:bg-stone-700 relative bg-gradient-to-b from-stone-700 to-stone-800 border-stone-900 text-stone-50 rounded-lg hover:bg-gradient-to-b hover:from-stone-800 hover:to-stone-800 hover:border-stone-900 after:absolute after:inset-0 after:rounded-[inherit] after:box-shadow after:shadow-[inset_0_1px_0px_rgba(255,255,255,0.25),inset_0_-2px_0px_rgba(0,0,0,0.35)] after:pointer-events-none transition antialiased">
-              Play Now
-            </a>
+      <div className={`${props.data.colour || 'bg-slate-100'} w-full rounded-lg shadow-xl shadow-black max-w-[30rem] overflow-hidden m-5 z-5`}>
+        {
+          (props.data.discord || props.data.steam) &&
+          <div className="flex items-center justify-end h-0 relative top-10 right-3">
             {
               props.data.discord &&
-              <a href={props.data.discord} target="_blank" rel="noopener noreferrer" className="m-1 inline-grid place-items-center border align-middle select-none font-sans font-medium text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-sm min-w-[38px] min-h-[38px] rounded-full shadow-sm hover:shadow-md border-[#24292e] bg-[#24292e] text-white hover:border-[#24292e] hover:bg-[#24292e] hover:brightness-110">
+              <a href={props.data.discord} target="_blank" rel="noopener noreferrer" className="m-1 inline-grid place-items-center border align-middle select-none font-sans font-medium text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-sm min-w-[48px] min-h-[48px] rounded-full shadow-sm hover:shadow-md border-[#24292e] bg-[#24292e] text-white hover:border-[#24292e] hover:bg-[#24292e] hover:brightness-110">
                 <img className='m-1.5' src={DiscordIcon} alt="discord" />
               </a>
             }
             {
               props.data.steam &&
-              <a href={props.data.steam} target="_blank" rel="noopener noreferrer" className="m-1 inline-grid place-items-center border align-middle select-none font-sans font-medium text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-sm min-w-[38px] min-h-[38px] rounded-full shadow-sm hover:shadow-md border-[#24292e] bg-[#24292e] text-white hover:border-[#24292e] hover:bg-[#24292e] hover:brightness-110">
+              <a href={props.data.steam} target="_blank" rel="noopener noreferrer" className="m-1 inline-grid place-items-center border align-middle select-none font-sans font-medium text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-sm min-w-[48px] min-h-[48px] rounded-full shadow-sm hover:shadow-md border-[#24292e] bg-[#24292e] text-white hover:border-[#24292e] hover:bg-[#24292e] hover:brightness-110">
                 <img className='m-1.5' src={SteamIcon} alt="steam" />
               </a>
             }
           </div>
+        }
+        <a href={props.data.link} target="_blank" rel="noopener noreferrer">
+          <img src={props.data.image} loading="lazy" className="w-[calc(100%-16px)] aspect-16/9 rounded m-2" />
+        </a>        
+        <div className="w-full h-max rounded px-3.5 py-2.5 text-center">
+          <h6 className="font-sans antialiased font-bold text-base md:text-lg lg:text-xl text-current">
+            {props.data.title}&nbsp;
+            <small className="font-sans antialiased text-sm text-current">
+              ({props.data.date})
+            </small>
+          </h6>
+          <p className="flex items-center justify-center font-sans antialiased text-base mt-1 text-stone-600 min-h-[3rem]">
+            {props.data.description}
+          </p>
         </div>
       </div>
     </>
